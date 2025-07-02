8e1d6911dbec895f91a8773fd68993a20034db3a
Viktor Medvid <darkissdark@gmail.com>
2025-07-02 17:43:26 +0200

    Configure Nuxt.js for client-side rendering and set base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    baseURL: "/",
+  },
 });
