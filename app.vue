3ae22b88393ddd37dc8674fef9c56a6dd6ff118d
Viktor Medvid <darkissdark@gmail.com>
2026-02-20 17:12:22 +0100

    Add ClientOnly wrapper around ThemeSwitcher for improved rendering in app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="text-foreground min-h-screen transition-colors duration-300 pb-4">
         <div class="fixed top-1 right-1 z-50">
-            <ThemeSwitcher />
+            <ClientOnly fallback-tag="div" fallback-class="p-2 rounded-full w-10 h-10">
+                <ThemeSwitcher />
+            </ClientOnly>
         </div>
         <NuxtRouteAnnouncer />
         <NuxtPage />
